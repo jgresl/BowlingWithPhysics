b7c37c4a70b1fd2a5fd5fe0451af900a6aa86b96
Jonathan Gresl <jgresl@hotmail.com>
2025-02-08 04:30:59 -0700

    Added check on ball launch to prevent multiple triggers

--- Assets/_Scripts/BallController.cs
+++ Assets/_Scripts/BallController.cs
@@ -5,13 +5,16 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
-    
+
+    // name booleans like a question
+    private bool isBallLaunched;
     private Rigidbody ballRB;
 
     void Start()
     {
         //Grabbing a reference to RigidBody
         ballRB = GetComponent<Rigidbody>();
+
 
         // Add a listener to the OnSpacePressed event.
         // When the space key is pressed the
@@ -22,6 +25,11 @@ public class BallController : MonoBehaviour
 
     private void LaunchBall()
     {
+        // now your if check can be framed like a sentence
+        // "if ball is launched, then simply return and do nothing"
+        if (isBallLaunched) return;
+        // "now that the ball is not launched, set it to true and launch the ball"
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
