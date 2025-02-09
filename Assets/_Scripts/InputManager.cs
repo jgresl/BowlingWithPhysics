b25aaac6d3824d16f278dafed383088cf7887c5f
Jonathan Gresl <jgresl@hotmail.com>
2025-02-09 01:41:59 -0700

    Updated inpute manager and created script to move player

--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -1,15 +1,30 @@
-using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnSpacePressed = new UnityEvent();
     void Update()
     {
+        
+        // Check and broadcast input for spacebar (to bowl)
         if (Input.GetKeyDown(KeyCode.Space))
         {
             OnSpacePressed?.Invoke();
         }
+
+        // Check and broadcast inputs for A and D keys (to move)
+        Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            input += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input += Vector2.right;
+        }
+
+        OnMove?.Invoke(input);
     }
 }
