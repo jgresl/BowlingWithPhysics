bc5645750485cd03fd3e071f29557e1d35645949
Jonathan Gresl <jgresl@hotmail.com>
2025-02-11 20:25:56 -0700

    Ensure pins only trigger fall if hitting the ground

--- Assets/_Scripts/FallTrigger.cs
+++ Assets/_Scripts/FallTrigger.cs
@@ -1,8 +1,6 @@
 using UnityEngine.Events;
 using UnityEngine;
 
-using UnityEngine.Events;
-
 public class FallTrigger : MonoBehaviour
 {
     // Adding an OnPinFall public event in case any other object
@@ -11,8 +9,15 @@ public class FallTrigger : MonoBehaviour
     public bool isPinFallen = false;
     private void OnTriggerEnter(Collider triggeredObject)
     {
-        isPinFallen = true;
-        OnPinFall?.Invoke();
-        Debug.Log($"{gameObject.name} is fallen");
+        // We can use the CompareTag() function
+        // to compare the collided object's tag to a string
+        // We also make sure that the OnPinFall is not invoked more than once
+        // For example if a pin were to bounce off the ground and hit it twice
+        if (triggeredObject.CompareTag("Ground") && !isPinFallen)
+        {
+            isPinFallen = true;
+            OnPinFall?.Invoke();
+            Debug.Log($"{gameObject.name} is fallen");
+        }
     }
 }
